0d8ba2becc2e9652f30b68fc4b2d0ca7259de0d5
Jordan Neal <jordaneal@gmail.com>
2021-10-11 20:53:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Import Dependencies
+var express = require("express");
+var app = express();
+var apiRoutes = require('./routes/api');
+var morgan = require('morgan');
+
+// Middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(morgan('tiny'));
+
+app.use(express.json());
+
+// Endpoints (commands)
+app.use('/api', apiRoutes);
+
+// Serve Out App
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening on ", host, port);
+});
